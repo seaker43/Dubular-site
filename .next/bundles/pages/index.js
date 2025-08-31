80137d4ce87631e3b0cb79a006eccdf22a0474a3
Your Name <youremail@example.comseaker4369@gmail.com>
2025-08-31 13:27:39 -0400

    Align beta build for Cloudflare Pages (Next-on-Pages)

--- .next/bundles/pages/index.js
+++
@@ -1,6 +0,0 @@
-
-          window.__NEXT_REGISTER_PAGE('/', function() {
-            var comp = module.exports=webpackJsonp([4],{153:function(e,t,u){"use strict";function n(){return r.default.createElement("h1",null,"Dubular is live!")}Object.defineProperty(t,"__esModule",{value:!0}),t.default=n;var l=u(14),r=function(e){return e&&e.__esModule?e:{default:e}}(l)},211:function(e,t,u){e.exports=u(153)}},[211]);
-            return { page: comp.default }
-          })
-        
--- README.md
+++
@@ -1,1 +0,0 @@
-# Dubular Beta
