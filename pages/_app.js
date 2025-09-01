805b1afa885634a0cb692c9b564d72fd2d713be0
seaker43 <seaker4343@gmail.com>
2025-09-01 15:56:08 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 // pages/_app.js
-import '../styles/beta.css';
-import '../styles/global.css'; // only if file exists
+import '../styles/global.css';
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
