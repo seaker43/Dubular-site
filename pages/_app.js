1d2ee117ada2ad5f1cb3dc4865697d97cc1ae328
Your Name <youremail@example.comseaker4369@gmail.com>
2025-09-01 11:29:21 -0400

    fix(next): de-duplicate _app.js (single default export)

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,5 @@
-// pages/_app.js
 import '../styles/beta.css';
-// If you don’t have globals.css, you can remove this line
-import '../styles/globals.css';
+import '../styles/globals.css'; // remove if you don't have this file
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
