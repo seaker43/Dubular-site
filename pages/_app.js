8fb52cbe6d9ddad107251215325924fa2e163c36
Your Name <youremail@example.comseaker4369@gmail.com>
2025-09-01 21:53:05 -0400

    fix: resolve merge markers in _app and index

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,7 @@
-<<<<<<< HEAD
 // pages/_app.js
-import "../styles/beta.css";
-import "../styles/global.css";
-import Header from "../components/Header";
-=======
 import '../styles/beta.css';
->>>>>>> e01c34ad (feat(ui): Leaderboards, Pools, Wallet + Edge API routes)
+import '../styles/global.css';
 
 export default function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Header />
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />;
 }
