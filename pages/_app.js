0dc252eadb9369a5e29a5451e5287b006eca0a9b
seaker43 <seaker4343@gmail.com>
2025-09-03 04:10:14 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import '../styles/globals.css'
+
 export default function App({ Component, pageProps }) {
   return (
     <>
       {/* Top logo header */}
       <header className="sticky top-0 z-40 h-12 flex items-center px-4 bg-[#080c11]/95 backdrop-blur border-b border-white/10">
         <div className="text-2xl font-extrabold tracking-wide text-white drop-shadow-[0_0_12px_#00e5ff]">
-          <span className="text-cyan-300">dub</span><span className="text-white">Ular</span>
+          <span className="text-cyan-300">dub</span>
+          <span className="text-white">Ular</span>
         </div>
       </header>
 
@@ -13,5 +15,5 @@ export default function App({ Component, pageProps }) {
 
       {/* Fixed bottom shortcuts */}
     </>
-  );
+  )
 }
