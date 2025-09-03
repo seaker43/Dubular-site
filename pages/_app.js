29028f7aebc4a55d602c2514ffa2861d86976e97
Your Name <youremail@example.comseaker4369@gmail.com>
2025-09-03 17:22:15 -0400

    fix(build): remove undefined DubularMark from _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,5 @@
-import "../styles/base.css";
-export default function App({Component, pageProps}){
-  return (
-    <>
-      <header className="hbar">
-        <div className="hrow container">
-          <DubularMark />
-        </div>
-      </header>
-      <Component {...pageProps}/>
-    </>
-  );
+import "../styles/globals.css";
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
 }
