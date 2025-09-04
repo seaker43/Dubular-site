bc3d160ce532c7d79a093184160c657e0bf92c89
Your Name <youremail@example.comseaker4369@gmail.com>
2025-09-04 01:14:22 -0400

    fix(ui): force remove duplicate plain text Dub U Lar in header

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,13 @@
 import Head from "next/head";
 import Link from "next/link";
-import DubularMark from "../ui/DubularMark";
 import Shelf from "../ui/Shelf";
 import demo from "../ui/demo";
 
 export default function Home() {
   return (
     <>
-      <Head><title>Dubular</title></Head>
       <header className="header">
         <div className="header-inner">
-          <Link href="/"><img src="/dubular-logo.svg" alt="Dubular" style={{height:54,display:"block"}}/></Link>
         </div>
       </header>
       <main className="main">
