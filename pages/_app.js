58e353ba508af1bef1e78999d8c9ba6cbfbb6149
Your Name <youremail@example.comseaker4369@gmail.com>
2025-09-04 01:15:48 -0400

    fix(ui): center neon Dubular wordmark in header with flex alignment

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,7 @@ import BottomBar from "../ui/BottomBar";
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <header className="header" style={{marginBottom:"0"}}>
+      <header className="header" style={{display:"flex",alignItems:"center",justifyContent:"center",marginBottom:"0"}} style={{marginBottom:"0"}}>
         <div className="header-inner" style={{justifyContent:"center"}}>
           </div>
       </header>
