1cf557d8ace0bf2904e91fb53961283058eeb607
Your Name <youremail@example.comseaker4369@gmail.com>
2025-09-03 23:53:12 -0400

    fix(ui): single neon Dubular wordmark with oversized U overlapping DUB, tight LAR

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,13 @@
 import "../styles/home.css";
-import Link from "next/link";
 import BottomBar from "../ui/BottomBar";
+import DubularWordmark from "../ui/DubularWordmark";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <header className="header">
         <div className="header-inner" style={{justifyContent:"center"}}>
-          <Link href="/"><img src="/dubular-logo.svg" alt="Dubular" style={{height:54,display:"block"}}/></Link>
+          <DubularWordmark />
         </div>
       </header>
       <Component {...pageProps} />
--- ui/DubularWordmark.jsx
+++ ui/DubularWordmark.jsx
@@ -1,6 +1,6 @@
 export default function DubularWordmark(){
   return (
-    <svg viewBox="0 0 600 120" xmlns="http://www.w3.org/2000/svg">
+    <svg viewBox="0 0 700 140" xmlns="http://www.w3.org/2000/svg">
       <defs>
         <linearGradient id="grad_cyan" x1="0%" y1="0%" x2="100%" y2="0%">
           <stop offset="0%" stopColor="#00f5ff"/>
@@ -8,9 +8,9 @@ export default function DubularWordmark(){
         </linearGradient>
         <filter id="glow"><feGaussianBlur stdDeviation="3" result="blur"/><feMerge><feMergeNode in="blur"/><feMergeNode in="SourceGraphic"/></feMerge></filter>
       </defs>
-      <text x="40" y="80" fontFamily="Arial Black, sans-serif" fontSize="58" fill="url(#grad_cyan)" filter="url(#glow)">DUB</text>
-      <text x="220" y="90" fontFamily="Arial Black, sans-serif" fontSize="86" fill="url(#grad_cyan)" filter="url(#glow)">U</text>
-      <text x="320" y="80" fontFamily="Arial Black, sans-serif" fontSize="58" fill="url(#grad_cyan)" filter="url(#glow)">LAR</text>
+      <text x="40" y="90" fontFamily="Arial Black, sans-serif" fontSize="64" fill="url(#grad_cyan)" filter="url(#glow)">DUB</text>
+      <text x="200" y="108" fontFamily="Arial Black, sans-serif" fontSize="110" fill="url(#grad_cyan)" filter="url(#glow)">U</text>
+      <text x="340" y="90" fontFamily="Arial Black, sans-serif" fontSize="64" fill="url(#grad_cyan)" filter="url(#glow)">LAR</text>
     </svg>
   );
 }
