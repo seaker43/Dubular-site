0092da1eb3fc7fdb32916d1abb156df7fa495a16
Your Name <youremail@example.comseaker4369@gmail.com>
2025-09-05 23:12:04 -0400

    ui: add fixed bottom shortcuts bar (safe-area aware) and include globally

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,11 @@
-import '../styles/globals.css';
 import "../styles/globals.css";
 import BottomBar from "../components/BottomBar";
 
 export default function App({ Component, pageProps }) {
   return (
-    <div className="min-h-screen bg-black text-green-100 pb-[92px]">
-      <main className="mx-auto w-full max-w-screen-sm px-2 pt-4">
-        <Component {...pageProps} />
-      </main>
+    <>
+      <Component {...pageProps} />
       <BottomBar />
-    </div>
+    </>
   );
 }
