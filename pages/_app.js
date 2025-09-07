dc28251fc19e51b74566e5060d0af3d92d9e9b7a
seaker43 <seaker4343@gmail.com>
2025-09-06 22:22:32 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 // pages/_app.js
 import "../styles/globals.css";
-import "../styles/theme-overrides.css"; // neon/glow overrides
+import "../styles/theme-overrides.css"; // neon/glow extras
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
