0cbee89c64fc25fb9a0a6e35d88b72156afe1e13
seaker43 <seaker4343@gmail.com>
2025-09-06 22:50:42 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,10 @@
 // pages/_app.js
-import "../styles/globals.css";
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <main className="bg-neutral-950 min-h-screen">
+      <Component {...pageProps} />
+    </main>
+  );
 }
