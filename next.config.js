bb76aad8108ae6841bd190a8ab397d329df980be
seaker43 <seaker4343@gmail.com>
2025-09-07 15:15:35 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
 /** @type {import("next").NextConfig} */
-const nextConfig={images:{domains:["images.unsplash.com","image.tmdb.org","i.ytimg.com","cdn.pixabay.com"]}};
-module.exports=nextConfig;
+const nextConfig = {
+  images: { domains: ["images.unsplash.com","image.tmdb.org","i.ytimg.com","cdn.pixabay.com"] }
+};
+module.exports = nextConfig;
