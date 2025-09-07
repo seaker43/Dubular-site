1e844ed514153dad10cca62c40ba4a316db2df7d
seaker43 <seaker4343@gmail.com>
2025-09-07 02:49:58 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,11 @@
-import '@/styles/globals.css'
-import BottomNav from '@/components/BottomNav'
+// pages/_app.js
+import "@/styles/globals.css";
+import Layout from "@/components/Layout";
 
-export default function App({ Component, pageProps, router }) {
-  // crude active detection from pathname
-  const path = router?.router?.pathname ?? router.pathname
-  const active =
-    path.startsWith('/ranks') ? 'ranks' :
-    path.startsWith('/favs') ? 'favs' :
-    path.startsWith('/wallet') ? 'wallet' :
-    path.startsWith('/account') ? 'account' :
-    'home'
-
+export default function App({ Component, pageProps }) {
   return (
-    <div className="pb-20"> {/* space for bottom nav */}
-      <main className="mx-auto max-w-5xl pt-6 space-y-8">
-        <Component {...pageProps} />
-      </main>
-      <BottomNav active={active} />
-    </div>
-  )
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
