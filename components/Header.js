dea7c34e8c7e89d4d05bec2ed07d572be15a485f
seaker43 <seaker4343@gmail.com>
2025-09-08 16:58:23 -0400

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,18 +1,25 @@
+// components/Header.jsx
 import Image from "next/image";
 import { forwardRef } from "react";
 
 const Header = forwardRef((props, ref) => {
   return (
-    <header ref={ref} className="site-header">
-      {/* logo is constrained by this box: 75% width, 90% height of header */}
-      <div className="relative w-3/4 h-[90%] flex items-center justify-center">
-        <Image
-          src="/Dubular2.png"
-          alt="dubUlar header logo"
-          fill
-          priority
-          className="object-contain select-none pointer-events-none"
-        />
+    <header
+      ref={ref}
+      className="site-header fixed top-0 left-0 right-0 z-50 bg-black/95 backdrop-blur h-20 overflow-hidden"
+    >
+      {/* full header box; center inner wrapper */}
+      <div className="relative w-full h-full flex items-center justify-center">
+        {/* logo wrapper: 75% width, 90% height of header */}
+        <div className="relative w-3/4 h-[90%]">
+          <Image
+            src="/Dubular2.png"
+            alt="dubUlar header logo"
+            fill
+            priority
+            className="object-contain select-none pointer-events-none"
+          />
+        </div>
       </div>
     </header>
   );
