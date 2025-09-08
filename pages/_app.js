a5e96a915a4c27716e9ca916d884fb98bf7dc457
seaker43 <seaker4343@gmail.com>
2025-09-07 21:04:45 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,5 @@
-// pages/_app.js
 import "@/styles/globals.css";
-import Layout from "@/components/Layout";
 
 export default function App({ Component, pageProps }) {
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  );
+  return <Component {...pageProps} />;
 }
