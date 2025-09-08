79922b5c6270916445ca91a1f5c07e460091798a
seaker43 <seaker4343@gmail.com>
2025-09-08 18:04:57 -0400

    Delete update_mainpage_netflix_style.sh

--- update_mainpage_netflix_style.sh
+++
@@ -1,7 +0,0 @@
-#!/data/data/com.termux/files/usr/bin/bash
-mkdir -p public && cd public
-for i in 1 2 3 4 5; do
-  curl -s -o "trending$i.jpg" "https://placehold.co/300x170?text=Thumbnail+$i"
-done
-cd ..
-git add .
