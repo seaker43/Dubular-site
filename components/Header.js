bf449ee51bbc738cf69cae6700ebbc24d6539eb3
seaker43 <seaker4343@gmail.com>
2025-09-08 13:42:34 -0400

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -3,14 +3,14 @@ header {
   @apply fixed top-0 left-0 right-0 z-50 
          bg-black/95 backdrop-blur 
          flex items-center justify-center;
-  height: 80px;       /* 10% shorter than 80px (h-20) */
+  height: 80px;
   border: none !important;
 }
 
 /* Logo inside header */
 header img {
   @apply block;
-  width: 90%;        /* 20% wider */
+  width: 90%;
   height: auto;
 }
 
