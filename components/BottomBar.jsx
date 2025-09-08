02192214c2c81cc162a2e29bb91a7d5995fd688b
seaker43 <seaker4343@gmail.com>
2025-09-07 23:18:51 -0400

    Update BottomBar.jsx

--- components/BottomBar.jsx
+++ components/BottomBar.jsx
@@ -1,34 +1,30 @@
 // components/BottomBar.jsx
 import Link from "next/link";
-import { useRouter } from "next/router";
 import { Trophy, Star, Home, Search, User } from "lucide-react";
 
 export default function BottomBar() {
-  const router = useRouter();
-
-  const Item = ({ href, icon: Icon, label }) => {
-    const active = router.pathname === href;
-    return (
-      <Link
-        href={href}
-        className={`navbar-item flex flex-col items-center ${
-          active ? "text-white" : "text-neutral-400"
-        }`}
-        aria-label={label}
-      >
-        <Icon size={20} strokeWidth={2} className="leading-none" />
-        <span className="text-xs mt-0.5">{label}</span>
-      </Link>
-    );
-  };
-
   return (
     <nav className="navbar">
-      <Item href="/rank" icon={Trophy} label="Rank" />
-      <Item href="/favorites" icon={Star} label="Favs" />
-      <Item href="/" icon={Home} label="Home" />
-      <Item href="/find" icon={Search} label="Find" />
-      <Item href="/account" icon={User} label="Account" />
+      <Link href="/rank" className="navbar-item">
+        <Trophy size={20} />
+        <span>Rank</span>
+      </Link>
+      <Link href="/favorites" className="navbar-item">
+        <Star size={20} />
+        <span>Favs</span>
+      </Link>
+      <Link href="/" className="navbar-item">
+        <Home size={20} />
+        <span>Home</span>
+      </Link>
+      <Link href="/find" className="navbar-item">
+        <Search size={20} />
+        <span>Find</span>
+      </Link>
+      <Link href="/account" className="navbar-item">
+        <User size={20} />
+        <span>Account</span>
+      </Link>
     </nav>
   );
 }
