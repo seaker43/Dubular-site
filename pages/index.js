690c9d311a514f8a1fa5e0bfa1eba5dbf0cc1ae4
seaker43 <seaker4343@gmail.com>
2025-09-07 21:44:57 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,25 +6,15 @@ export default function Home() {
   return (
     <Layout title="dubUlar">
       <div className="px-4 md:px-6 mt-1">
-        <FeaturedHero title="Featured Content" />
+        <FeaturedHero title="Featured Content" fullHeight />
       </div>
 
       <div className="px-4 md:px-6 mt-6">
         <MediaRow
           title="Live"
           items={[
-            {
-              id: 1,
-              title: "Live Music",
-              src: "/thumbnails/trending1.jpg",
-              live: true,
-            },
-            {
-              id: 2,
-              title: "Live Show",
-              src: "/thumbnails/trending2.jpg",
-              live: true,
-            },
+            { id: 1, title: "Live Music", image: "/thumbnails/trending1.jpg", live: true },
+            { id: 2, title: "Live Show",  image: "/thumbnails/trending2.jpg", live: true }
           ]}
         />
       </div>
