c3c306a30d737daba7dabd20ebd822e166b93181
seaker43 <seaker4343@gmail.com>
2025-09-08 09:17:38 -0400

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -6,7 +6,8 @@ export default function Layout({ children }) {
   return (
     <div className="bg-black min-h-screen flex flex-col">
       <Header />
-      <main className="flex-1 pt-32 px-4">  {/* 👈 pt-32 pushes content below logo */}
+      {/* No extra pt-* here; padding is controlled globally */}
+      <main className="flex-1 px-4 md:px-6">
         {children}
       </main>
       <BottomBar />
