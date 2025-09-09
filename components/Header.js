27390822a03fc5481417343ca2898f7d6959cb09
seaker43 <seaker4343@gmail.com>
2025-09-08 20:08:44 -0400

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -4,8 +4,8 @@ import Image from "next/image";
 export default function Header() {
   return (
     <header className="fixed top-0 left-0 right-0 z-50 bg-black/95 backdrop-blur h-20 flex items-center justify-center">
-      {/* Logo container: 75% width, full header height */}
-      <div className="relative w-[75%] h-full">
+      {/* Logo container: 60% width, full header height */}
+      <div className="relative w-[60%] h-full">
         <Image
           src="/Dubular2.v2.png"
           alt="dubUlar header logo"
