08a02c99a4495c56cdca373ba0650cc33f6a0aa5
seaker43 <seaker4343@gmail.com>
2025-09-11 16:44:17 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,11 @@
-import React from "react";
-import "../styles/header.css";
-import "../styles/layout.css";
-import "@/styles/globals.css";
-import "../styles/theme-overrides.css";
 // pages/_app.js
+import "@/styles/globals.css";
 import Layout from "@/components/Layout";
 
 export default function App({ Component, pageProps }) {
-  return <Layout><Component {...pageProps} /></Layout>;
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
