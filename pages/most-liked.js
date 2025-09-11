cc92ff68feebaaa8cb92306234e0c48cc2a26e83
Your Name <youremail@example.comseaker4369@gmail.com>
2025-09-11 19:32:51 -0400

    fix(pages): add default export + SSR for /most-liked

--- pages/most-liked.js
+++ pages/most-liked.js
@@ -1,11 +1,16 @@
 import Layout from "../components/Layout";
 
+export async function getServerSideProps() {
+  // SSR so CF Pages doesn’t attempt static export
+  return { props: {} };
+}
+
 export default function MostLikedPage() {
   return (
     <Layout title="Most Liked">
       <div className="p-4">
         <h1 className="text-2xl font-semibold">Most Liked</h1>
-        <p className="opacity-80 mt-2">This page is wired up and will render. Hook in your list UI when ready.</p>
+        {/* TODO: render your real list component here */}
       </div>
     </Layout>
   );
