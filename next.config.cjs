885e398b1cffbe46b46833f64924e8859b5f00ca
Your Name <youremail@example.comseaker4369@gmail.com>
2025-09-11 18:38:31 -0400

    prepare for cloudflare deploy

--- next.config.cjs
+++ next.config.cjs
@@ -1,8 +1,6 @@
-/** @type {import('next').NextConfig} */
+/** @type {import("next").NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: false,
-  images: { unoptimized: true },
-  experimental: { esmExternals: false },
+  swcMinify: true,
 };
 module.exports = nextConfig;
