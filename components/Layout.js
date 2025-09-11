c5e5b3359e817bd5011c3906917c0e63e505ebc6
seaker43 <seaker4343@gmail.com>
2025-09-11 16:00:27 -0400

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,6 @@
 import React from "react";
 // components/Layout.js
-import Header from './Header';
+import Header from './Header.js';
 import BottomBar from "./BottomBar";
 
 export default function Layout({ children }) {
