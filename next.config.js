86b9f770c66d13648a647b2084d44e95e45e0798
seaker43 <seaker4343@gmail.com>
2025-09-12 13:26:21 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,26 +1,16 @@
-// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  // prevent Next from forcing static 404/500 generation
-  output: 'standalone',
+
+  // Turn off SWC entirely (use Babel + Terser instead)
+  swcMinify: false,
+  experimental: {
+    swcLoader: false,
+    swcMinify: false,
+  },
 
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**', // allow all external image domains
-      },
-    ],
-    unoptimized: true, // skip Image Optimization (Cloudflare handles it)
-  },
-
-  eslint: {
-    ignoreDuringBuilds: true, // don’t block build on lint errors
-  },
-  typescript: {
-    ignoreBuildErrors: true, // don’t block build on TS errors
+    remotePatterns: [{ protocol: "https", hostname: "**" }],
   },
 };
 
