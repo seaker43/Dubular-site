55d62ec73bc77b8b00b8864460b858fc796974a6
Your Name <youremail@example.comseaker4369@gmail.com>
2025-09-12 18:20:24 -0400

    fix(app): dedupe MyApp and keep only globals.css

--- pages/_app.js
+++ pages/_app.js
@@ -3,12 +3,3 @@ import "../styles/globals.css";
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
-import Layout from "../components/Layout/Layout";
-
-export default function MyApp({ Component, pageProps }) {
- return (
- <Layout>
- <Component {...pageProps} />
- </Layout>
- );
-}
