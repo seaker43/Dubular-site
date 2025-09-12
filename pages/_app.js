35667720378fe643d42f9f3eeff04ff99d46591d
seaker43 <seaker4343@gmail.com>
2025-09-12 15:13:10 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,10 @@
-// pages/_app.js
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Layout from "../components/Layout/Layout";
 
 export default function MyApp({ Component, pageProps }) {
   return (
-    <main className="pt-0"> {/* or just remove className entirely */}
+    <Layout>
       <Component {...pageProps} />
-    </main>
-  )
+    </Layout>
+  );
 }
