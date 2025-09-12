3c0c8df4584feb6ae0cd7a3ae7c9852d6828ad93
seaker43 <seaker4343@gmail.com>
2025-09-12 10:26:56 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,15 @@
-import '../styles/globals.css'
+// pages/_app.js
+import "../styles/globals.css";
+import Header from "../components/Header";
+
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Header />
+      <main className="pt-[88px]"> 
+        {/* push content down so it's not hidden behind fixed header */}
+        <Component {...pageProps} />
+      </main>
+    </>
+  );
 }
