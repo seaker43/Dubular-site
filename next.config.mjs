ea998a119cdb3c6ed5c3d8d16f16898662a4b734
seaker43 <seaker4343@gmail.com>
2025-09-12 10:51:56 -0400

    Delete next.config.mjs

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: { unoptimized: true },        // good for CF Pages / next-on-pages
-  eslint: { ignoreDuringBuilds: true }, // silence ESLint during CF build
-  experimental: {
-  }
-};
-export default nextConfig;
