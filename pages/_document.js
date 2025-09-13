0dcfe9717befd12dc650aa33c54067a6d0a29ffc
Scott <seaker4369@gmail.com>
2025-09-12 21:02:34 -0400

    build: inject /overrides.css via _document for last-word CSS

---
+++ pages/_document.js
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        {/* Final overrides, load after Next CSS */}
+        <link rel="stylesheet" href="/overrides.css?v=1" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
