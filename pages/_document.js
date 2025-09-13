83859dc81f0e1fbdd3f277aa27131cf7b24e37bd
Scott <seaker4369@gmail.com>
2025-09-12 22:09:04 -0400

    chore: bump overrides.css to v=8 for cache bust

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
         {/* Final overrides, load after Next CSS */}
-        <link rel="stylesheet" href="/overrides.css?v=7" />
+        <link rel="stylesheet" href="/overrides.css?v=8" />
       </Head>
       <body>
         <Main />
