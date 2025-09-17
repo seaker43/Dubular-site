29a05b8baef65e6804d2bbd40514c86d8324d9cc
Scott <seaker4369@gmail.com>
2025-09-17 18:54:39 -0400

    fix: enforce no-store on all routes

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,12 @@
-import type { NextRequest } from 'next/server';
-import { NextResponse } from 'next/server';
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
-export const config = {
-  matcher: ['/((?!_next|.*\\.(?:js|css|png|jpg|svg|ico|txt)).*)'],
-};
-
-export default function middleware(_req: NextRequest) {
+export function middleware(req: NextRequest) {
   const res = NextResponse.next();
-  res.headers.set('Cache-Control', 'no-store');
+  res.headers.set("Cache-Control", "no-store, must-revalidate");
   return res;
 }
+
+export const config = {
+  matcher: ["/:path*"],
+};
