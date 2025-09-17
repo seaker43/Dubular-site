e0c91f6272c8a3b5385a24827d1e7e44acff7e48
Scott <seaker4369@gmail.com>
2025-09-17 15:42:22 -0400

    chore(opennext): add Cloudflare overrides

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,1 +1,28 @@
-import type { Config } from "open-next"; const config: Config = { default: {} }; export default config;
+import type { Config } from 'open-next';
+
+const config: Config = {
+  default: {
+    override: {
+      wrapper: 'cloudflare-node',
+      converter: 'edge',
+      proxyExternalRequest: 'fetch',
+      incrementalCache: 'dummy',
+      tagCache: 'dummy',
+      queue: 'direct'
+    }
+  },
+  middleware: {
+    external: true,
+    override: {
+      wrapper: 'cloudflare-edge',
+      converter: 'edge',
+      proxyExternalRequest: 'fetch',
+      incrementalCache: 'dummy',
+      tagCache: 'dummy',
+      queue: 'direct'
+    }
+  },
+  edgeExternals: ['node:crypto']
+};
+
+export default config;
