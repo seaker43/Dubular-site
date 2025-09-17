8bd704e87cfab3cb9e18e41640fe8e3085cdabb6
seaker43 <seaker4343@gmail.com>
2025-09-16 20:57:53 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,20 @@
-/** @type {import("next").NextConfig} */
-const nextConfig={eslint:{ignoreDuringBuilds:true},webpack:(config)=>{config.resolve=config.resolve||{};config.resolve.alias={...(config.resolve.alias??{})};return config;}};
-module.exports=nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  experimental: {
+    outputFileTracingRoot: __dirname,
+  },
+  // this is the critical part for Cloudflare Pages:
+  output: 'standalone',
+  webpack(config) {
+    config.resolve = config.resolve || {};
+    config.resolve.alias = {
+      ...(config.resolve.alias ?? {}),
+    };
+    return config;
+  },
+};
+
+module.exports = nextConfig;
