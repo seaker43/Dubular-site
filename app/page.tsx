eb6fa38e93e7cf5866ce1c4b14570b1e9f064786
Scott <seaker4369@gmail.com>
2025-09-17 18:22:25 -0400

    chore: force redeploy + no-store middleware

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
+export const dynamic="force-dynamic";
+export const fetchCache="force-no-store";
+export const revalidate=0;
+
 import HeadTags from "../components/HeadTags.jsx";
 import Header from "../components/Header.jsx";
 import HomeHero from "../components/HomeHero.js";
