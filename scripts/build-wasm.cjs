0ed842a16e3d0431f0ab3bf5124900d0a14b950a
seaker43 <seaker4343@gmail.com>
2025-09-18 16:53:37 -0400

    Delete scripts/build-wasm.cjs

--- scripts/build-wasm.cjs
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env node
-process.env.NEXT_DISABLE_SWC_BINARY = '1';
-process.env.NEXT_FORCE_WASM = '1';
-process.env.NEXT_SKIP_NATIVE_POSTINSTALL = '1';
-require('child_process').spawn('next', ['build'], { stdio: 'inherit', shell: true });
