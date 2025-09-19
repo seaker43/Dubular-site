dd79ce5f00ef29b8e09a3ed03998bb504d11f099
Scott <seaker4369@gmail.com>
2025-09-19 17:30:46 -0400

    chore: extend Tailwind theme with Dubular neon colors + shadows

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  theme: { extend: {} },
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        cyan: {
+          DEFAULT: "#22d3ee",
+          neon: "#00ffff",
+        },
+        neon: {
+          green: "#39ff14",
+          red: "#ff073a",
+        },
+      },
+      boxShadow: {
+        "neon-green": "0 0 8px #39ff14, 0 0 16px #39ff14",
+        "neon-cyan": "0 0 8px #00ffff, 0 0 16px #00ffff",
+      },
+    },
+  },
   plugins: [],
 };
