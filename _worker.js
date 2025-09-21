f5848cfbf2d166962e8779b186cc4cb4d67dfe03
Your Name <youremail@example.comseaker4369@gmail.com>
2025-09-21 18:05:26 -0400

    fix(cf): export OpenNext default fetch directly

--- _worker.js
+++ _worker.js
@@ -1,7 +1,1 @@
-import worker from "./.open-next/server-functions/default/index.js";
-
-export default {
-  async fetch(request, env, ctx) {
-    return worker.fetch(request, env, ctx);
-  },
-};
+export { default as fetch } from "./.open-next/server-functions/default/index.js";
