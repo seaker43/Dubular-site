092bc26406efca66a048edb6fdaaf67127d319ae
seaker43 <seaker4343@gmail.com>
2025-09-21 18:43:23 -0400

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,1 +1,6 @@
-export { default as fetch } from "./.open-next/server-functions/default/index.js";
+import worker from "./.open-next/server-functions/default/index.js";
+export default {
+  async fetch(request, env, ctx) {
+    return worker.fetch(request, env, ctx);
+  },
+};
