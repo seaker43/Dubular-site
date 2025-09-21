02afef77149700c2b363214f771ce394282f9293
root <root@localhost.localdomain>
2025-09-21 17:15:48 +0000

    include OpenNext outputs for deployment

--- .open-next/server-functions/default/.next/server/server-reference-manifest.js
+++ .open-next/server-functions/default/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"LQe3MZeYr2G2TQuCq07yLgt/aIsrm1BbzcOee5+n8sY=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"sYtYQo7u8PQbrZgW8OYSMiJ7V0Q+F2DBi8B/xgL3D3Q=\"}"
