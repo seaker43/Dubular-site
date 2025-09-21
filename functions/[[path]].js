08bfc3dc9cd5903e8656109d1e5e6c43514e7877
root <root@localhost.localdomain>
2025-09-21 18:51:22 +0000

    fix(cf): OpenNext cloudflare target

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -1,6 +1,1 @@
-import worker from '../.open-next/server-functions/default/index.mjs';
-
-// Adapt OpenNext's {fetch(req, env, ctx)} to Pages Functions' onRequest(ctx)
-export async function onRequest(context) {
-  return worker.fetch(context.request, context.env, { waitUntil: context.waitUntil });
-}
+import worker from '../.open-next/worker/index.mjs';export async function onRequest(c){return worker.fetch(c.request,c.env,{waitUntil:c.waitUntil})}
