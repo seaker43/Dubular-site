8beaea184622e38768eae88062179f9bd0940031
root <root@localhost.localdomain>
2025-09-21 18:54:46 +0000

    cf-pages: use root _worker.js to wrap OpenNext; remove Pages Functions

--- _worker.js
+++ _worker.js
@@ -1,2 +1,2 @@
-import worker from "./.open-next/worker.js";
-export default { fetch(request, env, ctx) { return worker.fetch(request, env, ctx); } };
+import worker from './.open-next/worker/index.mjs';
+export default { fetch: (req, env, ctx) => worker.fetch(req, env, ctx) };
--- functions/[[path]].js
+++
@@ -1,1 +0,0 @@
-import worker from '../.open-next/worker/index.mjs';export async function onRequest(c){return worker.fetch(c.request,c.env,{waitUntil:c.waitUntil})}
