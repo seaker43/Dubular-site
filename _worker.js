816a8427c809e96ae431cc76af2464c807794cf7
Your Name <youremail@example.comseaker4369@gmail.com>
2025-09-21 16:07:56 -0400

    fix(cf): reset wrangler, worker, and package.json scripts

--- _worker.js
+++ _worker.js
@@ -1,7 +1,6 @@
-import worker from "./.open-next/worker/index.mjs";
-
+import worker from './.open-next/worker/index.mjs';
 export default {
   async fetch(request, env, ctx) {
     return worker.fetch(request, env, ctx);
   }
-}
+};
