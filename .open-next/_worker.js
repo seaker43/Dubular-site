625ad421d2a78b97f3fc5a1dea4a70a5b4f98501
root <root@localhost.localdomain>
2025-09-22 04:50:05 +0000

    chore(git): track .open-next/_worker.js explicitly

--- .open-next/_worker.js
+++ .open-next/_worker.js
@@ -1,15 +1,14 @@
-
 import mod from "./server-functions/default/index.mjs";
-export const config = { compatibility_date: "2024-09-01" };
+
 export default {
   async fetch(req, env, ctx) {
-    const r = await mod.fetch(req, env, ctx);
-    const h = new Headers(r.headers);
-    h.set("x-worker", "active");
-    return new Response(r.body, {
-      status: r.status,
-      statusText: r.statusText,
-      headers: h
+    const res = await mod.fetch(req, env, ctx);
+    const headers = new Headers(res.headers);
+    headers.set("x-worker", "active");
+    return new Response(res.body, {
+      status: res.status,
+      statusText: res.statusText,
+      headers,
     });
-  }
-}
+  },
+};
