f0de15c02f73bae0160227de07a923db14ff50b7
root <root@localhost.localdomain>
2025-09-22 03:49:18 +0000

    feat(middleware): add x-worker header

---
+++ app/middleware.ts
@@ -0,0 +1,3 @@
+import { NextResponse } from 'next/server';
+export function middleware() { const res = NextResponse.next(); res.headers.set('x-worker','dubular-beta'); return res; }
+export const config = { matcher: '/:path*' };
