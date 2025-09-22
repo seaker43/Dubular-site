9f80ad4fede14274e6d69f9124d1a7b1e45683bd
root <root@localhost.localdomain>
2025-09-22 03:34:45 +0000

    chore(cf): enable nodejs_compat for Pages

--- _worker.js
+++ _worker.js
@@ -1,1 +1,3 @@
+export const config = { compatibility_date: "2024-09-01", compatibility_flags: ["nodejs_compat"] };
+export const config = { compatibility_date: "2024-09-01", compatibility_flags: ["nodejs_compat"] };
 import handler from "./.open-next/server-functions/default/index.mjs";export default {async fetch(req,env,ctx){let res=await handler.fetch(req,env,ctx);res=new Response(res.body,res);res.headers.set("x-debug-worker","on");res.headers.set("Cache-Control","no-store");return res;}};
