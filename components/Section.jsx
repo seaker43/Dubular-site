8deb96a6e4955fa3db33ccb8121d94576a3fced2
root <root@localhost.localdomain>
2025-09-23 18:03:43 +0000

    chore(cleanup): remove unused components

--- components/Section.jsx
+++
@@ -1,8 +0,0 @@
-export default function Section({ title, children }) {
-  return (
-    <section className="mt-8">
-      <h2 className="section-title safe-px mb-3">{title}</h2>
-      <div className="grid grid-cols-1 gap-4 safe-px">{children}</div>
-    </section>
-  );
-}
