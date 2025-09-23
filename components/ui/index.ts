0508ea902b044a97ce702c3d4face57e3db4f116
root <root@localhost.localdomain>
2025-09-23 19:08:25 +0000

    chore: add barrel exports for ui and layout

---
+++ components/ui/index.ts
@@ -0,0 +1,5 @@
+export { default as BottomBar } from "./ui/BottomBar";
+export { default as Header } from "./ui/Header";
+export { default as SearchBar } from "./ui/SearchBar";
+export { default as FeaturedHeroTabs } from "./ui/FeaturedHeroTabs";
+export { default as LiveRow } from "./ui/LiveRow";
