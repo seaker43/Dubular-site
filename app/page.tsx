e3827e007f77fa8d48166bdc8bd57eb2a6b2b512
Your Name <root@localhost.localdomain>
2025-09-24 00:44:58 -0400

    ui(home): style FeaturedHero w/ pink glow and LiveRow w/ red glow

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,12 @@ import LiveRow from "@/components/LiveRow";
 export default function HomePage() {
   return (
     <main className="flex flex-col gap-8 p-4">
-      <FeaturedHeroTabs />
-      <LiveRow />
+      <div className="rounded-lg shadow-[0_0_30px_10px_rgba(255,0,255,0.4)]">
+        <FeaturedHeroTabs />
+      </div>
+      <div className="rounded-lg shadow-[0_0_30px_10px_rgba(255,0,0,0.4)]">
+        <LiveRow />
+      </div>
     </main>
   );
 }
