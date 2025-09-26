a3e735a34cf86f40bc0f877d05853a7768940c77
Your Name <root@localhost.localdomain>
2025-09-26 14:14:55 -0400

    ui(header): set header + --header-h to 90px

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import Image from "next/image";
 export default function Header() {
   return (
-    <header className="bg-black sticky top-0 z-20 h-[112px] flex items-center justify-center">
+    <header className="bg-black sticky top-0 z-20 h-[90px] flex items-center justify-center">
       <Link href="/" className="bg-black flex items-center gap-2">
         <Image
           src="/logo.png"
