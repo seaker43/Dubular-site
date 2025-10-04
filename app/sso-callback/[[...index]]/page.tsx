d4403d415b8e9889096c6f9a57c5fd68eb210e0b
root <root@localhost.localdomain>
2025-10-04 05:04:36 +0000

    auth: add edge /sso-callback page for Clerk OAuth

--- app/sso-callback/[[...index]]/page.tsx
+++ app/sso-callback/[[...index]]/page.tsx
@@ -1,6 +1,6 @@
-"use client";
-export const runtime = "edge";
+"use client"
 import { AuthenticateWithRedirectCallback } from "@clerk/nextjs";
-export default function SSO() {
-  return <AuthenticateWithRedirectCallback redirectUrlComplete="/" />;
+export const runtime = "edge";
+export default function Page(){
+  return <AuthenticateWithRedirectCallback />;
 }
