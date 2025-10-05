13f22854496674369dd7381dcd85d3c63d2409bb
root <root@localhost.localdomain>
2025-10-05 06:53:24 +0000

    fix(auth): use Clerk <SignOutButton> with redirectUrl + click log

--- components/SignOutButton.tsx
+++ components/SignOutButton.tsx
@@ -1,20 +1,17 @@
-'use client';
-import { useClerk, ClerkLoaded } from '@clerk/nextjs';
-import { useRouter } from 'next/navigation';
+"use client";
+import { SignOutButton } from "@clerk/nextjs";
 
 export default function SignOutBtn() {
-  const { signOut } = useClerk();
-  const router = useRouter();
-
   return (
-    <ClerkLoaded>
+    <SignOutButton redirectUrl="/sign-in">
       <button
-        onClick={() => signOut(() => router.push('/sign-in'))}
-        className='mt-4 rounded-lg bg-[var(--laser-green,#00ff00)]/10 px-4 py-2 text-sm'
-        type='button'
+        type="button"
+        data-testid="signout-btn"
+        className="mt-4 rounded-lg px-4 py-2 text-sm border"
+        onClick={() => console.log("[signout] click")}
       >
         Sign out
       </button>
-    </ClerkLoaded>
+    </SignOutButton>
   );
 }
