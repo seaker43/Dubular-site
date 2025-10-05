0ac5ba64d8934a774ad3b548fd50129bb1f6ba99
seaker43 <seaker4343@gmail.com>
2025-10-05 01:17:59 -0400

    Delete components/SignOutButton.jsx

--- components/SignOutButton.jsx
+++
@@ -1,3 +0,0 @@
-"use client";
-import { SignedIn, SignOutButton } from "@clerk/nextjs";
-export default function SignOutBtn(){return (<SignedIn><SignOutButton redirectUrl="/sign-in"><button className="text-sm text-neutral-400 hover:text-white underline">Sign out</button></SignOutButton></SignedIn>);}
