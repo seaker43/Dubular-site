a74c7ddb1c0babb601db367af1e9b09488c62bf6
root <root@localhost.localdomain>
2025-10-06 01:54:08 +0000

    fix(account): Edge runtime + dynamic & no-cache

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,16 +1,15 @@
-export const dynamic = "force-dynamic";
+export const runtime = 'edge';
+export const dynamic = 'force-dynamic';
 export const revalidate = 0;
-export const fetchCache = "force-no-store";
+export const fetchCache = 'force-no-store';
 
-import { SignedIn, SignedOut, RedirectToSignIn } from "@clerk/nextjs";
-import AccountClient from "./page.client";
+import { SignedIn, SignedOut, RedirectToSignIn } from '@clerk/nextjs';
+import AccountClient from './page.client';
 
 export default function AccountPage() {
   return (
     <>
-      <SignedIn>
-        <AccountClient />
-      </SignedIn>
+      <SignedIn><AccountClient /></SignedIn>
       <SignedOut>
         <RedirectToSignIn redirectUrl="/sign-in" afterSignInUrl="/account" />
       </SignedOut>
