d58799f7e044a2cebed45a97f4e41514bb2d9719
root <root@localhost.localdomain>
2025-10-07 14:34:59 +0000

    chore: add /api/whoami debug endpoint

---
+++ app/api/whoami/route.ts
@@ -0,0 +1,1 @@
+export const runtime="edge";\nexport const dynamic="force-dynamic";\nimport { auth } from "@clerk/nextjs/server";\nexport async function GET(){const {userId}=auth();return new Response(JSON.stringify({userId}),{headers:{"content-type":"application/json"}});}
