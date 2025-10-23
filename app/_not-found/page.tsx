ff06537e63db7ba382a8a1b33a049f9f13bb70e0
root <root@localhost.localdomain>
2025-10-23 05:36:58 +0000

    fix(cloudflare): make /_not-found static but marked edge runtime

--- app/_not-found/page.tsx
+++ app/_not-found/page.tsx
@@ -1,1 +1,4 @@
-export const dynamic='force-static';\n\nexport default function GlobalNotFound(){return(<div className='flex min-h-screen items-center justify-center text-center text-xl'>Page not found</div>);}
+export const runtime='edge';
+export const dynamic='force-static';
+
+export default function GlobalNotFound(){return(<div className='flex min-h-screen items-center justify-center text-center text-xl'>Page not found</div>);}
